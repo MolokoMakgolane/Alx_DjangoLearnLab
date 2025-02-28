751a086d67129e0459f660bc9de18e5dd0531ad3
MolokoMakgolane <makgolanebatlilemoloko@gmail.com>
2025-02-28 12:16:21 +0200

    Update admin.py

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
 
-# Register your models here.
+
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
