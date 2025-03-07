76a18079cf1cdcc4b94ed7825807753a2b63e08d
MolokoMakgolane <makgolanebatlilemoloko@gmail.com>
2025-03-07 13:43:56 +0200

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -6,7 +6,7 @@ from .views import BookUpdateView
 from .views import BookDeleteView
 
 urlpatterns = [
-    path('books/', BookListView.as_view(), name="books-list"),
+    path('books/', BookList.as_view(), name="books-list"),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name="books-create"),
     path('books/update/', BookUpdateView.as_view(), name="books-update"),
