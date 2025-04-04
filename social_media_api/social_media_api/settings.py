87f786450361b711b350219fe7415fea632594d7
MolokoMakgolane <makgolanebatlilemoloko@gmail.com>
2025-04-04 15:15:28 +0200

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_django_project',
+        'USER': 'root',
+        'PASSWORD': '@#jsomething#',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
     }
 }
 
