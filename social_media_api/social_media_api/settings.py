30bca697cf17ac62e59db0901e72b680944e3b64
MolokoMakgolane <makgolanebatlilemoloko@gmail.com>
2025-04-04 15:09:31 +0200

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n5msja-ol*+$6g4zer#o8ox1)011bu93mh!3z3(dqdbf1%i-(w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
